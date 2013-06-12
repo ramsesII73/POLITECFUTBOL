b7f1dd7e536fddb65a594db770c189c819626bbf
Antonio Paternina <acpat88@gmail.com>
2013-06-12 14:57:37 -0500

    Creation of project

---
+++ POLITECFUTBOL/src/Cancha.java
@@ -0,0 +1,14 @@
+
+public class Cancha {
+	private String nombre;
+	private Sede sede;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Sede getSede() {
+		return sede;
+	}
+}
